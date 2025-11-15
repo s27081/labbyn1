06f72e8f013398d26e75fd137e7808daa10b919e
Patryk-Kosmider <s26985@pjwstk.edu.pl>
2025-11-15 11:51:49 +0100

    kan-80: Prometheus service controller unit tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,6 +1,8 @@
+"""Pytest configuration file for setting up test fixtures."""
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
+
 
 @pytest.fixture(scope="module")
 def test_client():
